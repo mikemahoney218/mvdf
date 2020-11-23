eabdc8186c1092b9ec9877cb0a7757f28a653600
Mike Mahoney <mike.mahoney.218@gmail.com>
2020-11-23 10:45:05 -0500

    Try fixing test again

--- tests/testthat/test-mvdf.R
+++ tests/testthat/test-mvdf.R
@@ -1,17 +1,19 @@
 test_that("mvdf are stable", {
 
   load("testdata/iris_test.rds")
-  mvdf(iris_test) <- mvdf(iris_test) # Convert idx to character on R < 4.0
   load("testdata/iris_mvdf.rds")
-  iris_mvdf$idx <- as.character(iris_mvdf$idx)
 
   expect_equal(
     iris_test,
     mvdf_obj(iris, Sepal.Width, Sepal.Length, Petal.Length, metadata = iris)
   )
 
+  # Fix test for R < 4.0
+  out_mvdf <- mvdf(iris_test)
+  out_mvdf$idx <- as.character(out_mvdf$idx)
+
   expect_equal(
     iris_mvdf,
-    mvdf(iris_test)
+    out_mvdf
   )
 })
