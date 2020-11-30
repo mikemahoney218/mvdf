a1d2d184eb90d33c01b1cbc5a5d3a9e6f236f530
Mike Mahoney <mike.mahoney.218@gmail.com>
2020-11-30 13:34:17 -0500

    Attempt to fix tests

--- tests/testthat/test-add_mesh_primitive.R
+++ tests/testthat/test-add_mesh_primitive.R
@@ -1,17 +1,19 @@
 test_that("add_mesh_primitive works", {
   iris_mvdf <- mvdf_obj(iris, Sepal.Width, Sepal.Length, Petal.Length)
   expect_equal(
-    gsub("\\r", "", create_blender_endmatter(
+    create_blender_endmatter(
       add_mesh_primitive(
         create_blender_frontmatter(),
         iris_mvdf,
         "ico_sphere"
       ),
       filepath = "tmp.blend"
-    )),
-    readChar(
-      "testdata/mesh_prim.txt",
-      file.info("testdata/mesh_prim.txt")$size
+    ),
+    gsub("\\r", "",
+         readChar(
+           "testdata/mesh_prim.txt",
+           file.info("testdata/mesh_prim.txt")$size
+           )
+         )
     )
-  )
 })
