bc580fed40fe2b1d223a220ff69ca8082dfeebcc
Mike Mahoney <mike.mahoney.218@gmail.com>
2020-11-30 12:35:28 -0500

    Add doc templates

---
+++ man-roxygen/amp_object.R
@@ -0,0 +1,3 @@
+#' @param object An object inheriting from [mvdf_obj] which will be used to
+#' calculate the x, y, and z positions for each primitive. One primitive will be
+#' created for each row of `mvdf(object)`.
---
+++ man-roxygen/script.R
@@ -0,0 +1,1 @@
+#' @param script The Python script to append the generated code onto.
