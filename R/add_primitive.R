3feabd43362df4fa74326fc8e474a70d3c3a9d1b
Mike Mahoney <mike.mahoney.218@gmail.com>
2021-01-05 09:48:27 -0500

    Use proceduralnames

--- R/add_primitive.R
+++ R/add_primitive.R
@@ -114,7 +114,7 @@ setMethod(
       "roughness"
     )])
     # TODO: Swap to proceduralnames
-    material_df$material_name <- uuid::UUIDgenerate(
+    material_df$material_name <- proceduralnames::make_english_names(
       n = nrow(material_df)
     )
     # This should almost never be tripped, but just in case we get extremely
@@ -122,7 +122,7 @@ setMethod(
     while (length(material_df$material_name) !=
       length(unique(material_df$material_name))) {
       # nocov start
-      material_df$material_name <- uuid::UUIDgenerate(
+      material_df$material_name <- proceduralnames::make_english_names(
         n = nrow(material_df)
       )
       # nocov end
